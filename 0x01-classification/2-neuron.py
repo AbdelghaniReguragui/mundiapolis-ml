ce7f8ebcaaa7cfea0c0abce562706c9c11da8cde
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-26 21:00:27 +0100

    Update 2-neuron.py

--- 0x01-classification/2-neuron.py
+++ 0x01-classification/2-neuron.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import numpy as np
+
 class Neuron:
     def __init__(self, nx):
         if type(nx) is not int:
@@ -9,6 +11,24 @@ class Neuron:
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
+
     def forward_prop(self, X):
         Y = np.matmul(self.__W, X) + self.__b
-        Glogistic = 1/(1+np.exp(-Y))
+        Glogistic = 1/(1+np.exp(-1*Y))
+
+    def forward_prop(self, X):
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A

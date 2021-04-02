48b21353bad4759776d84a0c75bc9e8ed5140b84
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-02 12:16:12 +0200

    Update 2-neuron.py

--- 0x01-classification/2-neuron.py
+++ 0x01-classification/2-neuron.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
 
-'''
-La classe Neuron 
-'''
+
 class Neuron:
-    '''
-    Le constructeur de la classe Neuron
-    '''
+
     def __init__(self, nx):
-        '''
-        Verification si nx est un entier
-        '''
+
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        '''
-        Verification si nx est plus grand de 1.
-        '''
+
         if nx < 1:
             raise ValueError("nx must be a positive integer")
             
@@ -24,18 +16,13 @@ class Neuron:
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-    
-    '''
-    La fonction forward_prop 
-    '''
+
     def forward_prop(self, X):
         preactivation = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-preactivation))
         return self.__A
-    
-''' 
-Setters
-'''
+
+
     @property
     def W(self):
         return self.__W

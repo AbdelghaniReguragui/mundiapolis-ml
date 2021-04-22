cdf13c68339ad7e6e2b883a79f5b557a1fedcbe7
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-22 13:27:31 +0200

    Update 0-create_placeholders.py

--- 0x02-tensorflow/0-create_placeholders.py
+++ 0x02-tensorflow/0-create_placeholders.py
@@ -1,1 +1,9 @@
 
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+def create_placeholders(nx, classes):
+    p1 = tf.placeholder("float", shape=[None, nx], name='x')
+    p2 = tf.placeholder("float", shape=[None, classes], name='y')
+    return p1, p2

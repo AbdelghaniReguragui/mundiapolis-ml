afdca0a39a313db3eb742b88f5f26a272136ff59
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-23 12:19:28 +0200

    Update 4-calculate_loss.py

--- 0x02-tensorflow/4-calculate_loss.py
+++ 0x02-tensorflow/4-calculate_loss.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 
 def calculate_loss(y, y_pred):
 
-    lo = tf.losses.softmax_cross_entropy(y, y_pred)
-    return lo
+    tmp = tf.losses.softmax_cross_entropy(y, y_pred)
+    return tmp

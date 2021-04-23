23ff3103029648ce9be3fb74db0318b9217db913
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-23 13:19:46 +0200

    Update 2-forward_prop.py

--- 0x02-tensorflow/2-forward_prop.py
+++ 0x02-tensorflow/2-forward_prop.py
@@ -6,13 +6,13 @@ import tensorflow as tf
 
 create_layer = __import__('1-create_layer').create_layer
 
-
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
     x: is the placeholder for the input data
     layer_sizes" is a list containing the number of nodes in each layer of the network
     activations: is a list containing the activation functions for each layer of the network
     Returns: the prediction of the network in tensor form
+    For this function, you should import your create_layer function with create_layer = __import__('1-create_layer').create_layer
     """
 
     if len(layer_sizes) and len(activations):

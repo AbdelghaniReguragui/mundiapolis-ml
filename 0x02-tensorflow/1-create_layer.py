334e014627ea06f9927f62f1a5ed01a2e3c544c5
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-23 13:11:26 +0200

    Update 1-create_layer.py

--- 0x02-tensorflow/1-create_layer.py
+++ 0x02-tensorflow/1-create_layer.py
@@ -3,6 +3,14 @@
 import tensorflow as tf
 
 def create_layer(prev, n, activation):
+    """
+    prev: is the tensor output of the previous layer
+    n: is the number of nodes in the layer to create
+    activation: is the activation function that the layer should use
+    tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG") to implement He et. al initialization for the layer weights
+    each layer have the name 'layer'
+    Returns: the tensor output of the layer
+    """
     i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     la = tf.layers.Dense(
         units=n,

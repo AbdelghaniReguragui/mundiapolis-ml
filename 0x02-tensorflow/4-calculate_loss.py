3a544f951f65e78bb15f58f254cbc69856590599
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-23 13:26:57 +0200

    Update 4-calculate_loss.py

--- 0x02-tensorflow/4-calculate_loss.py
+++ 0x02-tensorflow/4-calculate_loss.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+calculate loss
+"""
 import tensorflow as tf
 
 def calculate_loss(y, y_pred):
-
+    """
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the network’s predictions
+    Returns: a tensor containing the loss of the prediction
+    """
     tmp = tf.losses.softmax_cross_entropy(y, y_pred)
     return tmp

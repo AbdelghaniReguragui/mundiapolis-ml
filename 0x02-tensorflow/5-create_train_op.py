7da77c726b2ca42a18b0875b6ee68ebb1cb878ec
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-23 12:22:53 +0200

    Update 5-create_train_op.py

--- 0x02-tensorflow/5-create_train_op.py
+++ 0x02-tensorflow/5-create_train_op.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 
-
 def create_train_op(loss, alpha):
-
-    tmp = tf.train.GradientDescentOptimizer(alpha)
-    a = op.minimize(loss)
-    return a
+    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)

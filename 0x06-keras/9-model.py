ee8eae199ea15837b73fa5681941df7ed8dd7601
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:25:26 +0200

    Update 9-model.py

--- 0x06-keras/9-model.py
+++ 0x06-keras/9-model.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
+"""
 
+"""
 import tensorflow.keras as K
 
 def save_model(network, filename):
+    """
+    def save_model(network, filename): saves an entire model:
+    network is the model to save
+    filename is the path of the file that the model should be saved to
+    Returns: None
+    """
     network.save(filename)
 
 def load_model(filename):
+    """
+    def load_model(filename): loads an entire model:
+    filename is the path of the file that the model should be loaded from
+    Returns: the loaded model
+    """
     model = K.models.load_model('./' + filename)
     return model

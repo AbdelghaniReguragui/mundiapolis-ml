edbbad312b7efad0c809c3958739199546945006
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 15:41:23 +0200

    Update 1-input.py

--- 0x06-keras/1-input.py
+++ 0x06-keras/1-input.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
 
+import tensorflow.keras as K
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+
+    inputs = K.Input(shape=(nx,))
+    inp = inputs
+    for i in range(len(layers)):
+        if i + 1 < len(layers):
+            layer = K.layers.Dense(layers[i],
+                                   activation=activations[i],
+                                   kernel_regularizer=K.regularizers.l2(
+                                                        lambtha))(inp)
+            dropout = (K.layers.Dropout(1 - keep_prob))(layer)
+            inp = dropout
+        else:
+            layer = K.layers.Dense(layers[i],
+                                   activation=activations[i],
+                                   kernel_regularizer=K.regularizers.l2(
+                                                        lambtha))(inp)
+            output = layer
+    model = K.Model(inputs=inputs, outputs=output)
+    return model

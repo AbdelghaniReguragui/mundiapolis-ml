97ac0f0ba0dcc9d98fb3663dffd82150f1933fd4
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 15:24:51 +0200

    Update 7-evaluate.py

--- 0x02-tensorflow/7-evaluate.py
+++ 0x02-tensorflow/7-evaluate.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python3
+"""
+Evaluate
+"""
+import tensorflow as tf
 
+def evaluate(X, Y, save_path):
+  """
+  X is a numpy.ndarray containing the input data to evaluate
+  Y is a numpy.ndarray containing the one-hot labels for X
+  save_path is the location to load the model from
+  You are not allowed to use tf.saved_model
+  Returns: the network’s prediction, accuracy, and loss, respectively
+  """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(sess, save_path)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        y_eval = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy_eval = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_eval = sess.run(loss, feed_dict={x: X, y: Y})
+        return y_eval, accuracy_eval, loss_eval

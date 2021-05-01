1615bdfa5dea2852632ccfa1f8e75f6ecf81dd45
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:15:23 +0200

    Update 6-train.py

--- 0x06-keras/6-train.py
+++ 0x06-keras/6-train.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-
+"""
+Train 6
+"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
-
+    """
+    early_stopping is a boolean that indicates whether early stopping should be used
+    early stopping should only be performed if validation_data exists
+    early stopping should be based on validation loss
+    patience is the patience used for early stopping
+    """
     if validation_data:
         if early_stopping:
             call_back = [K.callbacks.EarlyStopping(monitor="val_loss",

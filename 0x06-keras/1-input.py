233399739377b1b5380d2f568aa85191c1755020
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:09:16 +0200

    Update 1-input.py

--- 0x06-keras/1-input.py
+++ 0x06-keras/1-input.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-
+"""
+Input 1
+"""
 import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-
+    """
+    nx is the number of input features to the network
+    layers is a list containing the number of nodes in each layer of the network
+    activations is a list containing the activation functions used for each layer of the network
+    lambtha is the L2 regularization parameter
+    keep_prob is the probability that a node will be kept for dropout
+    You are not allowed to use the Sequential class
+    Returns: the keras model
+    """
     inputs = K.Input(shape=(nx,))
     inp = inputs
     for i in range(len(layers)):

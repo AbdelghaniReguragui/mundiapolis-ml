87b43ec921fbdb62f9685a6582a6af2416f657c5
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:11:28 +0200

    Update 3-one_hot.py

--- 0x06-keras/3-one_hot.py
+++ 0x06-keras/3-one_hot.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-
+"""
+One hot
+"""
 import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
-
+    """
+    The last dimension of the one-hot matrix must be the number of classes
+    Returns: the one-hot matrix
+    """
     matrix = K.utils.to_categorical(labels, classes)
     return matrix

7467c96fc28ce2ba4987b532c5183537fbaecf1c
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:14:07 +0200

    Update 5-train.py

--- 0x06-keras/5-train.py
+++ 0x06-keras/5-train.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-
+"""
+Train5 
+"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
-  
+    """
+    validation_data is the data to validate the model with, if not None
+    """
     if validation_data:
         history = network.fit(data, labels, batch_size=batch_size,
                               epochs=epochs, verbose=verbose,

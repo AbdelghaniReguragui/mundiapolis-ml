295b8a7a6cb3373a5ca669f881e64a6a9a4e3e13
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 15:25:50 +0200

    Update 7-evaluate.py

--- 0x02-tensorflow/7-evaluate.py
+++ 0x02-tensorflow/7-evaluate.py
@@ -8,8 +8,6 @@ def evaluate(X, Y, save_path):
   """
   X is a numpy.ndarray containing the input data to evaluate
   Y is a numpy.ndarray containing the one-hot labels for X
-  save_path is the location to load the model from
-  You are not allowed to use tf.saved_model
   Returns: the network’s prediction, accuracy, and loss, respectively
   """
     with tf.Session() as sess:

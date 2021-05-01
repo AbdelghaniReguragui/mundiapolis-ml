71a1af7e74e98b93164e69a74c210ceae675f64c
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-01 16:01:43 +0200

    Update 11-config.py

--- 0x06-keras/11-config.py
+++ 0x06-keras/11-config.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
 
+
+def save_config(network, filename):
+    json_conf = network.to_json()
+    with open(filename, "w") as json_file:
+        json_file.write(json_conf)
+
+def load_config(filename):
+    with open(filename, "r") as json_file:
+        json_conf = json_file.read()
+    network = K.models.model_from_json(json_conf)
+    return network

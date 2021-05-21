8005c41c30900488737cf3d731b9abcfd506d3e2
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-21 10:21:11 +0200

    Update 8-train.py

--- 0x06-keras/8-train.py
+++ 0x06-keras/8-train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import tensorflow.keras as K
-
 """
 save_best is a boolean indicating whether to save the model after each epoch if it is the best
 a model is considered the best if its validation loss is the lowest that the model has obtained
@@ -21,23 +20,12 @@ def train_model(network, data, labels, batch_size, epochs,
     if validation_data:
         if early_stopping or learning_rate_decay:
             if early_stopping:
-                call_backs.append(K.callbacks.EarlyStopping(monitor="val_loss",
-                                                            patience=patience))
+                call_backs.append(K.callbacks.EarlyStopping(monitor="val_loss", patience=patience))
             if learning_rate_decay:
                 def scheduler(epoch):
-
                     return alpha * 1/(1 + decay_rate * epoch)
-                call_backs.append(
-                            K.callbacks.LearningRateScheduler(scheduler,
-                                                              verbose=True))
-        history = network.fit(data, labels, batch_size=batch_size,
-                              epochs=epochs, verbose=verbose,
-                              shuffle=shuffle,
-                              validation_data=validation_data,
-                              callbacks=call_backs)
+                call_backs.append(K.callbacks.LearningRateScheduler(scheduler, verbose=True))
+        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, validation_data=validation_data, callbacks=call_backs)
     else:
-        history = network.fit(data, labels, batch_size=batch_size,
-                              epochs=epochs, verbose=verbose,
-                              shuffle=shuffle,
-                              call_backs=call_backs)
+        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, call_backs=call_backs)
     return history

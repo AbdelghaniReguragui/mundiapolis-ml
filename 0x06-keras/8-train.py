06ccd8b468f532a9ba09fe374cd23c73a9d5759e
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-21 11:19:59 +0200

    Update 8-train.py

--- 0x06-keras/8-train.py
+++ 0x06-keras/8-train.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
+"""
+Train
+"""
 import tensorflow.keras as K
-"""
-save_best is a boolean indicating whether to save the model after each epoch if it is the best
-a model is considered the best if its validation loss is the lowest that the model has obtained
-filepath is the file path where the model should be saved
-"""
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False,
-                patience=0, learning_rate_decay=False, alpha=0.1,
-                decay_rate=1, save_best=False, filepath=None,
-                verbose=True, shuffle=False):
 
-    if filepath:
-        call_backs = [K.callbacks.ModelCheckpoint(filepath, monitor='val_loss',
-                                                  save_best_only=True)]
-    else:
-        call_backs = []
-    if validation_data:
-        if early_stopping or learning_rate_decay:
-            if early_stopping:
-                call_backs.append(K.callbacks.EarlyStopping(monitor="val_loss", patience=patience))
-            if learning_rate_decay:
-                def scheduler(epoch):
-                    return alpha * 1/(1 + decay_rate * epoch)
-                call_backs.append(K.callbacks.LearningRateScheduler(scheduler, verbose=True))
-        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, validation_data=validation_data, callbacks=call_backs)
-    else:
-        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle, call_backs=call_backs)
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None, verbose=True, shuffle=False):
+    """
+    save_best is a boolean indicating whether to save the model after each epoch if it is the best
+    a model is considered the best if its validation loss is the lowest that the model has obtained
+    filepath is the file path where the model should be saved
+    """
+
+    def l_r_decay(epoch):
+        """
+        
+        """
+        return alpha / (1 + decay_rate * epoch)
+
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(patience=patience,
+                                                   monitor="val_loss"))
+    if learning_rate_decay and validation_data:
+        callbacks.append(K.callbacks.LearningRateScheduler(l_r_decay,
+                                                           verbose=1))
+    if save_best and validation_data:
+        callbacks.append(K.callbacks.ModelCheckpoint(filepath=filepath,
+                                                     save_best_only=True))
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          verbose=verbose, validation_data=validation_data,
+                          shuffle=shuffle, callbacks=callbacks)
     return history

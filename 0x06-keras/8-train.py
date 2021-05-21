868e283ac10b51fd928c672cb06c7d3dbde1515f
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-21 10:24:13 +0200

    Update 8-train.py

--- 0x06-keras/8-train.py
+++ 0x06-keras/8-train.py
@@ -5,7 +5,6 @@ save_best is a boolean indicating whether to save the model after each epoch if 
 a model is considered the best if its validation loss is the lowest that the model has obtained
 filepath is the file path where the model should be saved
 """
-
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, learning_rate_decay=False, alpha=0.1,

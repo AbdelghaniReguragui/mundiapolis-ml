44d3f5ab13c1d3eeba50ece6890a91c26b8e0e9e
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 15:49:16 +0200

    Create 9-model.py

---
+++ 0x06-keras/9-model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import tensoflow.keras as tk
+
+def save_model(network, filename):
+  model.save('saved_model/my_model')
+  return None
+
+def load_model(filename):
+  loaded_model = tf.keras.models.load_model(filename)
+  return loaded_model

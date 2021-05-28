5fcc189a25e6d13d83a46cdfb1bc816f656aafed
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 16:22:15 +0200

    Create 10-weights.py

---
+++ 0x06-keras/10-weights.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import tensorflow.keras as tk
+
+def save_weights(network, filename, save_format='h5'):
+  network.save_weights(filepath, save_format=save_format)
+  return None
+
+def load_weights(network, filename):
+  network.load_weights(filepath)
+  return None

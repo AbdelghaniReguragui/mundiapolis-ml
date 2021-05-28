c78f365a35629751e8754dc760ae0ffc1567183a
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 16:32:02 +0200

    Update 10-weights.py

--- 0x06-keras/10-weights.py
+++ 0x06-keras/10-weights.py
@@ -14,11 +14,11 @@ def save_weights(network, filename, save_format='h5'):
     return None
 
 
+def load_weights(network, filename):
     """
     network is the model to which the weights should be loaded
     filename is the path of the file that the weights should be loaded from
     Returns: None
     """
-def load_weights(network, filename):
     network.load_weights('./' + filename)
     return None

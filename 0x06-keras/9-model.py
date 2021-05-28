72e6d08326aaff66e1bb5f5850883b6c595c47f4
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 15:59:32 +0200

    Update 9-model.py

--- 0x06-keras/9-model.py
+++ 0x06-keras/9-model.py
@@ -1,10 +1,11 @@
+
 #!/usr/bin/env python3
 import tensorflow.keras as tk
 
 def save_model(network, filename):
-  network.save(filename)
-  return None
+    network.save(filename)
+
 
 def load_model(filename):
-  network = tk.models.load_model(filename)
-  return network
+    model = tk.models.load_model(filename)
+    return model

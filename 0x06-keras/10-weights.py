a26dc82b45307f3b893239ec1c733e6bd8f86859
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 16:27:59 +0200

    Update 10-weights.py

--- 0x06-keras/10-weights.py
+++ 0x06-keras/10-weights.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """
 Save and Load Weights
-
 """
 import tensorflow.keras as K
 
 """
-def save_weights(network, filename, save_format='h5'): saves a model’s weights:
+saves a model’s weights:
 network is the model whose weights should be saved
 filename is the path of the file that the weights should be saved to
 save_format is the format in which the weights should be saved
@@ -17,7 +16,7 @@ def save_weights(network, filename, save_format='h5'):
     return None
 
 """
-def load_weights(network, filename): loads a model’s weights:
+loads a model’s weights:
 network is the model to which the weights should be loaded
 filename is the path of the file that the weights should be loaded from
 Returns: None

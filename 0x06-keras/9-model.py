8192c0f4787e3ce7de7f5e7745a655af63130c48
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-05-28 15:51:55 +0200

    Update 9-model.py

--- 0x06-keras/9-model.py
+++ 0x06-keras/9-model.py
@@ -2,9 +2,9 @@
 import tensoflow.keras as tk
 
 def save_model(network, filename):
-  model.save('saved_model/my_model')
+  model.save(filename)
   return None
 
 def load_model(filename):
-  loaded_model = tk.models.load_model(filename)
-  return loaded_model
+  network = tk.models.load_model(filename)
+  return network

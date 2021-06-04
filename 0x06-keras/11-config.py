ab2ccad8d9cf75621c6809482a95ceff0e02b4a4
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-06-04 10:34:11 +0200

    Update 11-config.py

--- 0x06-keras/11-config.py
+++ 0x06-keras/11-config.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
+""" Save and Load Configuration """
+
 import tensorflow.keras as K
 
 def save_config(network, filename):
+  """
+  saves a model’s configuration in JSON format:
+  network is the model whose configuration should be saved
+  filename is the path of the file that the configuration should be saved to
+  Returns: None
+  """
   model_json = network.to_json()
   with open(filename, "w") as json_file:
-    json_file.write(model_json)    
+    json_file.write(model_json)
   return none
 
 def load_config(filename):
+  """
+   loads a model with a specific configuration:
+  filename is the path of the file containing the model’s configuration in JSON format
+  Returns: the loaded model
+  """
   with open(filename, "r") as json_file:
     loaded_model_json = json_file.read()
   network = K.models.model_from_json(loaded_model_json)

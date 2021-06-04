4f74a67abf910e458b767ac399b2b15701168f28
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-06-04 10:26:44 +0200

    Update 11-config.py

--- 0x06-keras/11-config.py
+++ 0x06-keras/11-config.py
@@ -3,12 +3,12 @@ import tensorflow.keras as K
 
 def save_config(network, filename):
   model_json = network.to_json()
-  with open("/"+ filename, "w") as json_file:
+  with open(filename, "w") as json_file:
     json_file.write(model_json)
   return none;
 
 def load_config(filename):
-  with open("/"+filename, 'r') as json_file:
+  with open(filename, 'r') as json_file:
     loaded_model_json = json_file.read()
   network = K.models.model_from_json(loaded_model_json)
   return network
